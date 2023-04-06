a373f59783b0c5351c17c9fa0dd51326d6285660
arash.esmaeilian <arash.esmaelian@zoodfood.com>
2023-04-06 21:20:48 +0430

    features tab now changes when clicking on each tab

---
+++ src/scripts/featuresSlider.js
@@ -0,0 +1,26 @@
+const tabs = document.querySelectorAll(".features-element");
+const illustrationTab = document.querySelectorAll(".illustration-tab1>img");
+const descriptions = document.querySelectorAll("main>section:nth-child(5)>div");
+const redUnderline = document.querySelectorAll(".red-underline");
+/* ***LOGS*** */
+console.log(tabs);
+console.log(illustrationTab);
+console.log(descriptions);
+console.log(redUnderline);
+/* ***LOGS*** */
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (onClickEvent) => {
+    /* inserting underline */
+    redUnderline.forEach((underline) => (underline.style.display = "none"));
+    redUnderline[index].style.display = "block";
+
+    /* changing illustration image */
+    illustrationTab.forEach((image) => (image.style.display = "none"));
+    illustrationTab[index].style.display = "block";
+
+    /* changing tab description */
+    descriptions.forEach((des) => (des.style.display = "none"));
+    descriptions[index].style.display = "flex";
+  });
+});
