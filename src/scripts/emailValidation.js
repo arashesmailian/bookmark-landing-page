569411344377c146b303c735ac28b20dfb3f370f
arash.esmaeilian <arash.esmaelian@zoodfood.com>
2023-04-07 00:04:24 +0430

    some styles added

--- src/scripts/emailValidation.js
+++ src/scripts/emailValidation.js
@@ -3,7 +3,6 @@ const submitBtn = document.querySelector("form button");
 const inputContainer = document.querySelector(".input-container");
 const exclamationIcon = document.querySelector(".input-container span");
 const errorText = document.querySelector(".input-container em");
-//
 
 let regExp =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -13,19 +12,28 @@ function check() {
     inputContainer.style.padding = "0";
     exclamationIcon.style.display = "none";
     errorText.style.display = "none";
-    submitBtn.disabled = false;
+    submitBtn.style.marginBottom = "0";
+    submitBtn.style.color = "white";
+    submitBtn.style.background = "hsl(0, 94%, 66%)";
+    submitBtn.style.border = "none";
   } else {
     exclamationIcon.style.display = "block";
     inputContainer.style.background = "hsl(0, 94%, 66%)";
-    inputContainer.style.padding = "3px 3px 5px 3px";
+    inputContainer.style.padding = "3px 0px 5px 3px";
     errorText.style.display = "block";
-    submitBtn.disabled = true;
+    submitBtn.style.marginBottom = "28px";
+    submitBtn.style.color = "hsl(0, 94%, 66%)";
+    submitBtn.style.background = "white";
+    submitBtn.style.border = "2px solid hsl(0, 94%, 66%)";
   }
   if (email.value == "") {
     inputContainer.style.background = "inherit";
     inputContainer.style.padding = "0";
     exclamationIcon.style.display = "none";
     errorText.style.display = "none";
-    submitBtn.disabled = false;
+    submitBtn.style.marginBottom = "0";
+    submitBtn.style.color = "white";
+    submitBtn.style.background = "hsl(0, 94%, 66%)";
+    submitBtn.style.border = "none";
   }
 }
